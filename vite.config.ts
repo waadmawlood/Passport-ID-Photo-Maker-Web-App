ddc24aa7e22db7fe8264a38ccb7793876df81055
Waad Mawlood <w12345678996392@gmail.com>
2026-07-17 23:21:13 +0300

    chore: configure base path for deployment in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Passport-ID-Photo-Maker-Web-App/',
   plugins: [react(), tailwindcss()],
 })
